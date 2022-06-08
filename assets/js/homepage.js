8ff5f9a07d6854f8e2230dd3d77ce095224d03ca
kfisch2 <kmlindsey24@gmail.com>
2022-06-08 10:45:20 -0700

    function to retrieve github user repos

---
+++ assets/js/homepage.js
@@ -0,0 +1,12 @@
+let getUsersRepos = function (user) {
+  // format github api url 
+  let apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+};
+
